465236c05525fa73f2f16d273eccee541972b5ca
Ibraheam <iheemoo@gmail.com>
2017-10-08 13:12:16 +0300

    First commit

---
+++ src/main/java/co/akwad/asksaudi/AsksaudiApplication.java
@@ -0,0 +1,12 @@
+package co.akwad.asksaudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsksaudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsksaudiApplication.class, args);
+	}
+}
